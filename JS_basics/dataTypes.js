1f652b82486a904a4665a29963ec1e52b81b3176
dcparas99-alt <dcparas99@gmail.com>
2025-11-07 07:37:53 +0545

    abc

---
+++ JS_basics/dataTypes.js
@@ -0,0 +1,27 @@
+// const name = "Techspire";
+// const age = 2;
+// const yourAgeGreaterThan21 = true
+// const myDetails = {
+  
+// }
+
+// const animal = "Dog";
+
+// console.log(animal);
+
+// abc();
+
+// function abc() {
+//     const flower = "Rose";
+//     console.log(flower);
+// }
+// const abc2 = () => {
+//     const flower = "Rose";
+//     console.log(flower);
+// }
+// abc2();
+
+const userName = () => {
+    console.log("Pasang DC")
+};
+userName();
